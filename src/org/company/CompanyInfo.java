528ec8adc8de91c38410624ea233a817416e43f5
Divya-chandran <divyaofficial2122@gmail.com>
2022-03-05 20:39:55 +0530

    DIVYA

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -10,6 +10,10 @@ private void companyId() {
 private void companyAddress() { 
 	System.out.println("Jurong East, Singapore");
 }
+private void employee() {
+	System.out.println("Employee strength is 2000");
+
+}
 public static void main(String[] args) {
 	CompanyInfo RNC = new CompanyInfo ();
 	RNC.companyName();
